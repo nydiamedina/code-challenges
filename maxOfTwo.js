025f69dd467be3083741c59538ce9adf3014facd
Nydia Medina <nvmedina29@gmail.com>
2021-11-16 18:53:56 -0600

    First commit, including maxOfTwo challenge

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 2
+let y = 500
+
+// Write your code below this line.
+
+if (x > y) {
+  console.log(x);
+} else if (x < y) {
+  console.log(y);
+} else {
+  console.log('The values are identical.');
+}
