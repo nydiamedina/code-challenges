43572f93045d6b7d9fb330f15473b4fd86159ff6
Nydia Medina <nvmedina29@gmail.com>
2021-12-04 18:49:07 -0600

    Submitting Find Lucky Numbers challenge

---
+++ findLuckyNumbers.js
@@ -0,0 +1,18 @@
+// Write your code below this line.
+const min = 1;
+const max = 10;
+
+const findLuckyNumbers = n => {
+  let luckyNumbers = [];
+  for (let i = 0; i < n; i++) {
+    let newLuckyNumber = Math.floor(Math.random() * (max - min + 1) + min);    
+    
+    while (luckyNumbers.includes(newLuckyNumber)) {
+      newLuckyNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    luckyNumbers.push(newLuckyNumber);
+  }
+  return luckyNumbers;
+}
+
+console.log(findLuckyNumbers(10));
