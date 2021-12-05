b65069a52db55bd66f8fe610350a5fdba59ffd46
Nydia Medina <nvmedina29@gmail.com>
2021-12-04 19:11:30 -0600

    Changing scope of constants in Find Lucky Numbers challenge

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,9 +1,9 @@
 // Write your code below this line.
-const min = 1;
-const max = 10;
-
 const findLuckyNumbers = n => {
+  const min = 1;
+  const max = 10;  
   let luckyNumbers = [];
+
   for (let i = 0; i < n; i++) {
     let newLuckyNumber = Math.floor(Math.random() * (max - min + 1) + min);    
     
@@ -12,6 +12,7 @@ const findLuckyNumbers = n => {
     }
     luckyNumbers.push(newLuckyNumber);
   }
+  
   return luckyNumbers;
 }
 
